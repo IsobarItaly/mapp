05ccb084e06fef7549c7c5ccb1e08753587f2564
Gabriele Stefanini <gabriele.stefanini@isobar.com>
2022-01-13 14:47:11 +0100

    first release

---
+++ README.md
@@ -0,0 +1,60 @@
+# MAPP Integration w/ Laravel
+
+Add in `.env`
+
+```bash
+MAPP_URL=https://up.to/api/version
+MAPP_USERNAME=
+MAPP_PASSWORD=
+```
+
+Add in `config/services.php`
+
+```php
+// ...
+'mapp' => [
+    'url' => env('MAPP_URL', ''),
+    'username' => env('MAPP_USERNAME', ''),
+    'password' => env('MAPP_PASSWORD', ''),
+],
+// ...
+```
+
+
+Modify `app/Providers/AppServiceProvider.php`
+
+```php
+
+use Dentsu\MAPP\MAPP;
+
+//... inside register
+
+$this->app->singleton(
+    MAPP::class, 
+    fn() => new MAPP(
+        config('services.mapp.url'),
+        config('services.mapp.username'),
+        config('services.mapp.password')
+    )
+);
+```
+
+Use it in Dependency Injection for example in controller
+
+```php
+use Dentsu\MAPP\MAPP;
+
+// ...
+public function __construct(private MAPP $mapp) {}
+// ...
+public function example(string $email)
+{
+    $parameters = [
+        'param1' => 'value1',
+        'param2' => 'value2',
+        'param3' => 'value3',
+    ];
+  
+    $this->mapp->sendEmail($email, $templateID, $parameters);
+}
+```
---
+++ src/MAPP/MAPP.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\MAPP\MAPPUser;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class MAPP
+{
+    public function __construct(
+        private string $endpoint,
+        private string $username,
+        private string $password
+    ) {
+        //
+    }
+
+    public function firstOrCreate(string $email, array $payload = []): MAPPUser
+    {
+//            $payload['user.identifier'] = $userId;
+        $mappUser = $this->getByEmail($email);
+        if ($mappUser === null) {
+            $mappUser = $this->userCreate($email, $payload);
+        }
+
+        return $mappUser;
+    }
+
+    public function getByEmail(string $email): ?MAPPUser
+    {
+        $res = $this->getClient()
+            ->get(
+                $this->endpoint . '/user/getByEmail',
+                [
+                    'email' => $email,
+                ]
+            );
+        Log::info(json_encode($res->json()));
+
+        if (isset($res->json()['errorActor'])) {
+            return null;
+        }
+
+        return new MAPPUser(...$res->json());
+    }
+
+    public function userCreate(string $email, array $payload): MAPPUser
+    {
+        $res = $this->getClient()
+            ->post(
+                $this->endpoint . '/user/create?email=' . $email,
+                $this->buildPayload($payload)
+            );
+        if (isset($res->json()['errorActor'])) {
+            Log::info(__METHOD__);
+            Log::info('error on ' . $this->endpoint . '/user/create?email=' . $email);
+        }
+        return new MAPPUser(...$res->json());
+    }
+
+    public function sendEmail(string $email, int $templateId, array $parameters)
+    {
+        Log::debug(__METHOD__ . ' _ START');
+        $mappUser = $this->firstOrCreate($email);
+        Log::debug(json_encode($mappUser));
+        $data = [
+            'recipientId' => $mappUser->id,
+            'messageId' => $templateId,
+        ];
+        Log::debug(json_encode($data));
+        Log::debug(json_encode($this->buildPayload($parameters)));
+        $res = $this->getClient()
+            ->post(
+                $this->endpoint . '/message/sendSingle?' . http_build_query($data),
+                ['parameters' => $this->buildPayload($parameters)]
+            );
+        Log::debug(json_encode($res->json()));
+        Log::debug(__METHOD__ . ' _ END');
+    }
+
+    private function buildPayload(array $payload)
+    {
+        $mappPayload = [];
+        foreach ($payload as $key => $value) {
+            $mappPayload[] = ['name' => $key, 'value' => $value];
+        }
+
+        return $mappPayload;
+    }
+
+    private function getClient(): PendingRequest
+    {
+        return Http::withBasicAuth(
+            $this->username,
+            $this->password
+        )
+            ->acceptJson();
+    }
+}
